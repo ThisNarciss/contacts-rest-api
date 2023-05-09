76f612b6fe9669bbe711612e3fdb6f5a3b77ac80
ThisNarciss <tyomaxxx000@gmail.com>
2023-05-09 16:52:07 +0300

    add controllers and validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,87 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { nanoid } = require("nanoid");
-const contacts = require("../../models/contacts.js");
-const utils = require("../../utils");
+const ctrl = require("../../controllers/contacts");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", ctrl.getAll);
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const result = await contacts.getContactById(req.params.contactId);
-    if (!result) {
-      throw utils.HttpError(404, "Not found");
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:contactId", ctrl.getById);
 
-router.post("/", async (req, res, next) => {
-  console.log(req.body);
-  try {
-    utils.addContactValid(req.body);
-    const newContact = { id: nanoid(), ...req.body };
-    const result = await contacts.addContact(newContact);
-    res.json({
-      status: "success",
-      code: 201,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", ctrl.add);
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const result = await contacts.removeContact(req.params.contactId);
-    if (!result) {
-      throw utils.HttpError(404, "Not found");
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      message: "contact deleted",
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId", ctrl.del);
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    utils.updateContactValid(req.body);
-    const result = await contacts.updateContact(req.params.contactId, req.body);
-    if (!result) {
-      throw utils.HttpError(404, "Not found");
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:contactId", ctrl.update);
 
 module.exports = router;
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,10 @@
 const HttpError = require("./HttpError");
 const { addContactValid, updateContactValid } = require("./validations");
+const ctrlWrapper = require("./ctrlWrapper");
 
-module.exports = { HttpError, addContactValid, updateContactValid };
+module.exports = {
+  HttpError,
+  addContactValid,
+  updateContactValid,
+  ctrlWrapper,
+};
