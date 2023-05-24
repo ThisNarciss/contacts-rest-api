f80949cea495e098cb789f4674aaa665229e9676
ThisNarciss <tyomaxxx000@gmail.com>
2023-05-24 08:52:54 +0300

    add verification in middleware

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,7 +7,7 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, _, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" && !token) {
     next(HttpError(401, "Not authorized"));
   }
   try {
